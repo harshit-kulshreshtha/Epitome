1335cd2f69a014c547fe4e2354c40b3bbd78a3b4
harshit kulshreshtha <harshitkul44@gmail.com>
2024-05-25 20:34:59 +0530

    Setting up NextJs

---
+++ epitomart/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main> 
+      <nav></nav>
+      <h1>Epitome</h1>
+      <footer></footer>
+    </main>
+  );
+}
